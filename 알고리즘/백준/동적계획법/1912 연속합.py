e68587ae6bc08d65226a382675558b7e69027536
HG-Shin <sinhuigwan7@naver.com>
2024-02-13 23:36:47 +0900

    동적 계획법 공부중

--- 알고리즘/백준/동적계획법/1912 연속합.py
+++ 알고리즘/백준/동적계획법/1912 연속합.py
@@ -1,14 +1,7 @@
 n = int(input())
 num = list(map(int, input().split()))
 
-dp = [0] * n
-dp[n-1] = num[n-1]
-dp[n-2] = num[n-2] + dp[n-1]
-dp[n-3] = num[n-3] + dp[n-2]
+for i in range(1, n):
+    num[i] = max(num[i-1]+num[i], num[i])
 
-for i in range(n-4, -1, -1):
-    dp[i] = num[i] + dp[i+1]
-    
-print(max(dp))
-
-# 내일 풀 예정
+print(max(num))
