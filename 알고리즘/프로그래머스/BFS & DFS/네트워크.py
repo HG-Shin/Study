498b614dcca0cbdf77f1f33d3ac247a958d32c93
HG-Shin <sinhuigwan7@naver.com>
2024-02-24 01:00:43 +0900

    SQL 및 알고리즘 공부

---
+++ 알고리즘/프로그래머스/BFS & DFS/네트워크.py
@@ -0,0 +1,18 @@
+def solution(n, computers):
+    answer = 0
+    
+    def dfs(node):
+        visited[node] = 1
+        
+        for i in range(n):
+            if computers[node][i] == 1 and visited[i] == 0:
+                dfs(i)
+    
+    visited = [0] * n
+    
+    for i in range(n):
+        if visited[i] == 0:
+            dfs(i)
+            answer += 1
+    
+    return answer
